0386f021ba1b4ce277e270ae677f2351ee87da9a
jayasurya018 <jayasuryavs1999@gmail.com>
2022-04-28 11:01:34 +0530

    updated about

--- src/About.js
+++ src/About.js
@@ -14,36 +14,31 @@ function About() {
     <div className="center gap">
       <a
         target={"_blank"}
-        href="https://www.linkedin.com/in/prithiviraj-mern-fullstack"
+        href="https://www.linkedin.com/in/jayasurya-vs-a4ba25216/"
       >
         <Fab color="primary" aria-label="add">
           <LinkedInIcon />
         </Fab>
       </a>
-        <a
-          target={"_blank"}
-          href="https://github.com/Prithiv11">
+      <a target={"_blank"} href="https://github.com/JAYASURYA018">
         <Fab color="error" aria-label="add">
-          <GitHubIcon/>
+          <GitHubIcon />
+        </Fab>
+      </a>
+      <a target={"_blank"} href="https://twitter.com/jayasuryavs1999">
+        <Fab color="warning" aria-label="add">
+          <TwitterIcon />
+        </Fab>
+      </a>
+      <a target={"_blank"} href="https://www.instagram.com/jaya_surya_vs/">
+        <Fab color="secondary" aria-label="add">
+          <InstagramIcon />
         </Fab>
       </a>
       <a
         target={"_blank"}
-        href="https://twitter.com/prithiviraj624"
+        href="https://www.facebook.com/profile.php?id=100063623570303"
       >
-      <Fab color="warning" aria-label="add">
-        <TwitterIcon />
-      </Fab></a>
-      <a
-        target={"_blank"}
-        href="https://www.instagram.com/prithiv_here/"
-      >
-      <Fab color="secondary" aria-label="add">
-        <InstagramIcon />
-      </Fab></a>
-      <a
-        target={"_blank"}
-        href="https://www.facebook.com/prithiviraj.thala.7">
         <Fab color="primary" aria-label="add">
           <FacebookIcon />
         </Fab>
